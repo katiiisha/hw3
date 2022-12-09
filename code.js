c24ff1876c4f4e68e391285934aefa2ae778b09b
katiiisha <mendeleevakate2002@gmail.com>
2022-12-09 03:31:02 +0300

    выполнила дз

---
+++ code.js
@@ -0,0 +1,16 @@
+//задача 1
+let arr = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12]
+for (i = 0; i < arr.length; i++) { 
+    if ((arr[i] > 0) && (arr[i] < 10)) { 
+        console.log(arr[i])
+    }
+}
+//задача2 
+let arr1 = [];
+for (i = 0; i < 11; i++) { 
+    arr1[i] = 'x'
+}
+console.log(arr1)
+
+
+
